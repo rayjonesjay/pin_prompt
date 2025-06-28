979f1630b0642ccf832fac5a7c63c17126d5590a
Ray Jones <150661844+rayjonesjay@users.noreply.github.com>
2025-06-28 22:32:44 +0300

    Update Landing Page with Slogan and Mobile Fixes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  trailingSlash: true,
+  trailingSlash: false,
   // Remove output: 'export' to allow dynamic routes
 };
 
